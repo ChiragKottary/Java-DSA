e0e7ce18c8ae2e00096d74e245f188f01d015a62
Chirag kottary <ckottary18@gmail.com>
2024-03-31 20:38:40 +0530

    day-2

---
+++ src/addNumbers.java
@@ -0,0 +1,12 @@
+public class addNumbers {
+    public static void main(String[] args) {
+
+        int res=add(23,54);
+        System.out.println(res);
+    }
+    static int add(int a , int b){
+        int result = a+b;
+        return result;
+    }
+
+}
